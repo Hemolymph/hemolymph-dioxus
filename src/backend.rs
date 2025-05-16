015e15af9c7087bbf0171268febcbc7c7546a170
Katie Ampersand <katie-and@ampersandia.net>
2025-05-16 15:55:35 -0500

    Better debouncing

--- src/backend.rs
+++ src/backend.rs
@@ -1,7 +1,8 @@
 use notify::RecursiveMode;
-use notify_debouncer_mini::new_debouncer;
+use notify_debouncer_mini::{new_debouncer, DebounceEventResult};
+use std::fmt::Debug;
 use std::path::Path;
-use std::sync::mpsc::TryRecvError;
+use std::sync::LazyLock;
 use std::{
     collections::HashMap,
     fs,
@@ -13,13 +14,8 @@ use dioxus::prelude::*;
 use hemoglobin::cards::Card;
 
 #[cfg(feature = "server")]
-thread_local! {
-    pub static CARDS: Arc<RwLock<HashMap<String, Card>>> = {
-        let data = fs::read_to_string("../hemolymph-static/files/cards.json").expect("Unable to read cards.json");
-        let cards = serde_json::from_str::<Vec<Card>>(&data).unwrap();
-        Arc::new(RwLock::new(create_card_map(cards)))
-    };
-}
+pub static CARDS: LazyLock<Arc<RwLock<HashMap<String, Card>>>> =
+    LazyLock::new(|| Arc::new(RwLock::new(HashMap::new())));
 
 #[server(endpoint = "search")]
 pub async fn process_query(query: String) -> Result<Vec<Card>, ServerFnError> {
@@ -29,27 +25,25 @@ pub async fn process_query(query: String) -> Result<Vec<Card>, ServerFnError> {
 
     let query_s = hemoglobin::search::query_parser::parse_query(&query);
     let query = query_s.map_err(|x| ServerFnError::new("Failed to parse query."))?;
-    let cards: Vec<_> = CARDS.with(|cards| {
-        let cards = cards.read().unwrap();
+    let cards: Vec<_> = {
+        let cards = CARDS.read().unwrap();
         hemoglobin::search::search(&query, cards.values())
             .into_iter()
             .cloned()
             .collect()
-    });
+    };
 
     Ok(cards)
 }
 
 #[server(endpoint = "card")]
 pub async fn get_card_id(id: String) -> Result<Card, ServerFnError> {
-    CARDS.with(|cards| {
-        cards
-            .read()
-            .unwrap()
-            .get(&id)
-            .ok_or(ServerFnError::new("Id does not exist"))
-            .cloned()
-    })
+    CARDS
+        .read()
+        .unwrap()
+        .get(&id)
+        .ok_or(ServerFnError::new("Id does not exist"))
+        .cloned()
 }
 
 #[cfg(feature = "server")]
@@ -61,46 +55,73 @@ fn create_card_map(vec: Vec<Card>) -> HashMap<String, Card> {
 pub fn setup_card_debounce() {
     use std::thread::sleep;
 
+    match load_cards_json() {
+        Ok(()) => println!("Successful first load of cards.json"),
+        Err(LoadError::IoError(err)) => {
+            eprintln!("Failed to read cards.json: {err:#?} from disk on first initialization")
+        }
+        Err(LoadError::SerdeJsonError(err)) => {
+            eprintln!("Failed to deserialize cards.json: {err:#?} on first initialization")
+        }
+    }
+
     std::thread::spawn(|| {
-        let (tx, rx) = std::sync::mpsc::channel();
-        let mut debouncer = new_debouncer(Duration::from_secs(1), tx).unwrap();
-        debouncer
-            .watcher()
-            .watch(
-                Path::new("../hemolymph-static/files"),
-                RecursiveMode::Recursive,
-            )
-            .unwrap();
+        let mut debouncer = new_debouncer(Duration::from_secs(1), watcher_response).unwrap();
         loop {
-            match rx.try_recv() {
-                Ok(Ok(events)) => {
-                    for event in events {
-                        if event.path.ends_with("cards.json") {
-                            match fs::read_to_string("../hemolymph-static/files/cards.json") {
-                                Ok(data) => match serde_json::from_str::<Vec<Card>>(&data) {
-                                    Ok(data) => {
-                                        CARDS.with(|cards| {
-                                            let mut cards = cards.write().unwrap();
-                                            *cards = create_card_map(data);
-                                        });
-                                        // let mut cards = CARDS.write().await;
-                                        // *cards = create_card_map(data);
-                                        println!("Successfully reloaded cards.json");
-                                    }
-                                    Err(x) => eprintln!("Failed to load cards.json: {x:#?}"),
-                                },
-                                Err(x) => eprintln!("Failed to read cards.json: {x:#?}"),
-                            }
+            debouncer
+                .watcher()
+                .watch(
+                    Path::new("../hemolymph-static/files"),
+                    RecursiveMode::Recursive,
+                )
+                .unwrap();
+
+            sleep(Duration::from_millis(10));
+        }
+    });
+}
+
+#[derive(Debug)]
+enum LoadError {
+    IoError(std::io::Error),
+    SerdeJsonError(serde_json::Error),
+}
+
+#[cfg(feature = "server")]
+fn watcher_response(events: DebounceEventResult) {
+    match events {
+        Ok(events) => {
+            for event in events {
+                if event.path.ends_with("cards.json") {
+                    match load_cards_json() {
+                        Ok(()) => println!("Successfully reloaded cards.json"),
+                        Err(LoadError::IoError(err)) => {
+                            eprintln!(
+                                "Failed to read cards.json: {err:#?} from disk after debounce"
+                            )
+                        }
+                        Err(LoadError::SerdeJsonError(err)) => {
+                            eprintln!("Failed to deserialize cards.json: {err:#?} after debounce")
                         }
                     }
                 }
-                Ok(Err(error)) => eprintln!("Failed to watch: {error:#?}"),
-                Err(TryRecvError::Disconnected) => {
-                    eprintln!("File watcher was disconnected. This should not happen.");
-                }
-                Err(TryRecvError::Empty) => (),
             }
-            sleep(Duration::from_secs(0));
         }
-    });
+        Err(error) => eprintln!("Failed to watch: {error:#?}"),
+    }
+}
+
+#[cfg(feature = "server")]
+fn load_cards_json() -> Result<(), LoadError> {
+    match fs::read_to_string("../hemolymph-static/files/cards.json") {
+        Ok(data) => match serde_json::from_str::<Vec<Card>>(&data) {
+            Ok(data) => {
+                let mut cards = CARDS.write().unwrap();
+                *cards = create_card_map(data);
+                Ok(())
+            }
+            Err(x) => Err(LoadError::SerdeJsonError(x)),
+        },
+        Err(x) => Err(LoadError::IoError(x)),
+    }
 }
